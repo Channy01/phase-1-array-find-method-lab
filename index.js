371f5d87045f52da97c0563053910a1f9435366e
ChantiaKindle <chantia.kindle0123@gmail.com>
2022-04-10 22:30:34 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    const superbowl = record.find(element => element.result === "W")
+    if (superbowl) {
+        return superbowl.year
+    }
+    else { return undefined }
+}
